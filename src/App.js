61043e9887d717a0cb39a2f76d5609e51a9de8b3
leminhhieu98py <leminhhieu98py@gmail.com>
2022-09-26 22:17:03 +0700

    handle button state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import './App.css';
+import { PRIMARY_COLOR, SECONDARY_COLOR } from './color';
 
 function App() {
+  const [color, setColor] = useState(PRIMARY_COLOR);
+
   return (
     <div>
-      <button>Change to red</button>
+      <button
+        onClick={() =>
+          setColor(color === PRIMARY_COLOR ? SECONDARY_COLOR : PRIMARY_COLOR)
+        }
+        style={{ backgroundColor: color, color: '#fff' }}
+      >
+        Change to {color === PRIMARY_COLOR ? SECONDARY_COLOR : PRIMARY_COLOR}
+      </button>
     </div>
   );
 }
