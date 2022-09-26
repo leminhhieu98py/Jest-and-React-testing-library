5f4dba243e14932d3fdcc8472c167a08e33e8d9a
leminhhieu98py <leminhhieu98py@gmail.com>
2022-09-26 22:53:26 +0700

    Add checkbox feature

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import { PRIMARY_COLOR, SECONDARY_COLOR } from './color';
 
 function App() {
   const [color, setColor] = useState(PRIMARY_COLOR);
+  const [isCheck, setIsCheck] = useState(false);
 
   return (
     <div>
+      <input type="checkbox" onClick={(e) => setIsCheck(e.target.checked)} />
       <button
         onClick={() =>
           setColor(color === PRIMARY_COLOR ? SECONDARY_COLOR : PRIMARY_COLOR)
         }
         style={{ backgroundColor: color, color: '#fff' }}
+        disabled={isCheck}
       >
         Change to {color === PRIMARY_COLOR ? SECONDARY_COLOR : PRIMARY_COLOR}
       </button>
