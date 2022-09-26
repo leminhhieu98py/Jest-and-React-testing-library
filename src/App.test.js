1e8b9ed21d5e02c5ae8e43b4f075cb183598a3ff
leminhhieu98py <leminhhieu98py@gmail.com>
2022-09-26 22:17:16 +0700

    write unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import { PRIMARY_COLOR, SECONDARY_COLOR } from './color';
 
-test('renders learn react link', () => {
+test('Button at initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react 2/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByRole('button', {
+    name: `Change to ${SECONDARY_COLOR}`
+  });
+  expect(buttonElement).toHaveStyle({ backgroundColor: PRIMARY_COLOR });
+});
+
+test('Button toggle color', () => {
+  render(<App />);
+  const buttonElement = screen.getByRole('button', {
+    name: `Change to ${SECONDARY_COLOR}`
+  });
+  expect(buttonElement).toHaveStyle({ backgroundColor: PRIMARY_COLOR });
+
+  fireEvent.click(buttonElement);
+  expect(buttonElement).toHaveStyle({ backgroundColor: SECONDARY_COLOR });
+
+  fireEvent.click(buttonElement);
+  expect(buttonElement).toHaveStyle({ backgroundColor: PRIMARY_COLOR });
 });
