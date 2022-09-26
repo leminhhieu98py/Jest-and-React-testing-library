4815e73c06b10568bbdc46b7c0ecc8c504e11c9c
leminhhieu98py <leminhhieu98py@gmail.com>
2022-09-26 22:53:45 +0700

    Add unit test for checkbox feature

--- src/App.test.js
+++ src/App.test.js
@@ -26,3 +26,30 @@ test('Button toggle color', () => {
   expect(buttonElement.textContent).toBe(`Change to ${SECONDARY_COLOR}`);
   expect(buttonElement).toHaveStyle({ backgroundColor: PRIMARY_COLOR });
 });
+
+test('Toggle disable button', () => {
+  render(<App />);
+  const buttonElement = screen.getByRole('button', {
+    name: `Change to ${SECONDARY_COLOR}`
+  });
+  const checkboxElement = screen.getByRole('checkbox');
+  expect(buttonElement).toBeEnabled();
+
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeDisabled();
+
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeEnabled();
+});
+
+test('Toggle click checkbox', () => {
+  render(<App />);
+  const checkboxElement = screen.getByRole('checkbox');
+  expect(checkboxElement).not.toBeChecked();
+
+  fireEvent.click(checkboxElement);
+  expect(checkboxElement).toBeChecked();
+
+  fireEvent.click(checkboxElement);
+  expect(checkboxElement).not.toBeChecked();
+});
