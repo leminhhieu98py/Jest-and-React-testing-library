0172f04d2bab099b2b9c6fb20c887cfceae77ec4
leminhhieu98py <leminhhieu98py@gmail.com>
2022-09-29 23:07:05 +0700

    comment test and replace order summary page to App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import { useState } from 'react';
-import './App.css';
-import { PRIMARY_COLOR, SECONDARY_COLOR, DISABLE_COLOR } from './color';
+// import './App.css';
+// import { Button } from 'antd';
+// import { useState } from 'react';
+// import { PRIMARY_COLOR, SECONDARY_COLOR, DISABLE_COLOR } from './color';
 
-function App() {
-  const [color, setColor] = useState(PRIMARY_COLOR);
-  const [isCheck, setIsCheck] = useState(false);
-  const newButtonColor =
-    color === PRIMARY_COLOR ? SECONDARY_COLOR : PRIMARY_COLOR;
+import OrderSummaryPage from './pages/order-summary/OrderSummaryPage';
 
-  return (
-    <div>
-      <input type="checkbox" onClick={(e) => setIsCheck(e.target.checked)} />
-      <button
-        onClick={() => setColor(newButtonColor)}
-        style={{
-          backgroundColor: isCheck ? DISABLE_COLOR : color,
-          color: '#fff'
-        }}
-        disabled={isCheck}
-      >
-        Change to {newButtonColor}
-      </button>
-    </div>
-  );
-}
+// function App() {
+//   const [color, setColor] = useState(PRIMARY_COLOR);
+//   const [isCheck, setIsCheck] = useState(false);
+//   const newButtonColor =
+//     color === PRIMARY_COLOR ? SECONDARY_COLOR : PRIMARY_COLOR;
+
+//   return (
+//     <div>
+//       <input type="checkbox" onClick={(e) => setIsCheck(e.target.checked)} />
+//       <button
+//         onClick={() => setColor(newButtonColor)}
+//         style={{
+//           backgroundColor: isCheck ? DISABLE_COLOR : color,
+//           color: '#fff'
+//         }}
+//         disabled={isCheck}
+//       >
+//         Change to {newButtonColor}
+//       </button>
+//       <Button type="primary">Primary Button</Button>
+//     </div>
+//   );
+// }
+
+const App = () => {
+  return <OrderSummaryPage />;
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,58 +1,58 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import App from './App';
-import { PRIMARY_COLOR, SECONDARY_COLOR, DISABLE_COLOR } from './color';
+// import { fireEvent, render, screen } from '@testing-library/react';
+// import App from './App';
+// import { PRIMARY_COLOR, SECONDARY_COLOR, DISABLE_COLOR } from './color';
 
-test('Button at initial color', () => {
-  render(<App />);
-  const buttonElement = screen.getByRole('button', {
-    name: `Change to ${SECONDARY_COLOR}`
-  });
-  expect(buttonElement).toHaveTextContent(`Change to ${SECONDARY_COLOR}`);
-  expect(buttonElement).toHaveStyle({ backgroundColor: PRIMARY_COLOR });
-});
+// test('Button at initial color', () => {
+//   render(<App />);
+//   const buttonElement = screen.getByRole('button', {
+//     name: `Change to ${SECONDARY_COLOR}`
+//   });
+//   expect(buttonElement).toHaveTextContent(`Change to ${SECONDARY_COLOR}`);
+//   expect(buttonElement).toHaveStyle({ backgroundColor: PRIMARY_COLOR });
+// });
 
-test('Button toggle color', () => {
-  render(<App />);
-  const buttonElement = screen.getByRole('button', {
-    name: `Change to ${SECONDARY_COLOR}`
-  });
-  expect(buttonElement).toHaveStyle({ backgroundColor: PRIMARY_COLOR });
+// test('Button toggle color', () => {
+//   render(<App />);
+//   const buttonElement = screen.getByRole('button', {
+//     name: `Change to ${SECONDARY_COLOR}`
+//   });
+//   expect(buttonElement).toHaveStyle({ backgroundColor: PRIMARY_COLOR });
 
-  fireEvent.click(buttonElement);
-  expect(buttonElement).toHaveTextContent(`Change to ${PRIMARY_COLOR}`);
-  expect(buttonElement).toHaveStyle({ backgroundColor: SECONDARY_COLOR });
+//   fireEvent.click(buttonElement);
+//   expect(buttonElement).toHaveTextContent(`Change to ${PRIMARY_COLOR}`);
+//   expect(buttonElement).toHaveStyle({ backgroundColor: SECONDARY_COLOR });
 
-  fireEvent.click(buttonElement);
-  expect(buttonElement).toHaveTextContent(`Change to ${SECONDARY_COLOR}`);
-  expect(buttonElement).toHaveStyle({ backgroundColor: PRIMARY_COLOR });
-});
+//   fireEvent.click(buttonElement);
+//   expect(buttonElement).toHaveTextContent(`Change to ${SECONDARY_COLOR}`);
+//   expect(buttonElement).toHaveStyle({ backgroundColor: PRIMARY_COLOR });
+// });
 
-test('Toggle disable button', () => {
-  render(<App />);
-  const buttonElement = screen.getByRole('button', {
-    name: `Change to ${SECONDARY_COLOR}`
-  });
-  const checkboxElement = screen.getByRole('checkbox');
-  expect(buttonElement).toBeEnabled();
-  expect(buttonElement).toHaveStyle({ backgroundColor: PRIMARY_COLOR });
+// test('Toggle disable button', () => {
+//   render(<App />);
+//   const buttonElement = screen.getByRole('button', {
+//     name: `Change to ${SECONDARY_COLOR}`
+//   });
+//   const checkboxElement = screen.getByRole('checkbox');
+//   expect(buttonElement).toBeEnabled();
+//   expect(buttonElement).toHaveStyle({ backgroundColor: PRIMARY_COLOR });
 
-  fireEvent.click(checkboxElement);
-  expect(buttonElement).toBeDisabled();
-  expect(buttonElement).toHaveStyle({ backgroundColor: DISABLE_COLOR });
+//   fireEvent.click(checkboxElement);
+//   expect(buttonElement).toBeDisabled();
+//   expect(buttonElement).toHaveStyle({ backgroundColor: DISABLE_COLOR });
 
-  fireEvent.click(checkboxElement);
-  expect(buttonElement).toBeEnabled();
-  expect(buttonElement).toHaveStyle({ backgroundColor: PRIMARY_COLOR });
-});
+//   fireEvent.click(checkboxElement);
+//   expect(buttonElement).toBeEnabled();
+//   expect(buttonElement).toHaveStyle({ backgroundColor: PRIMARY_COLOR });
+// });
 
-test('Toggle click checkbox', () => {
-  render(<App />);
-  const checkboxElement = screen.getByRole('checkbox');
-  expect(checkboxElement).not.toBeChecked();
+// test('Toggle click checkbox', () => {
+//   render(<App />);
+//   const checkboxElement = screen.getByRole('checkbox');
+//   expect(checkboxElement).not.toBeChecked();
 
-  fireEvent.click(checkboxElement);
-  expect(checkboxElement).toBeChecked();
+//   fireEvent.click(checkboxElement);
+//   expect(checkboxElement).toBeChecked();
 
-  fireEvent.click(checkboxElement);
-  expect(checkboxElement).not.toBeChecked();
-});
+//   fireEvent.click(checkboxElement);
+//   expect(checkboxElement).not.toBeChecked();
+// });
