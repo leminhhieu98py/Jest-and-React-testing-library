eca34f6f511c9695ea5be0767a9cd5fcb103f6de
leminhhieu98py <leminhhieu98py@gmail.com>
2022-09-29 23:07:32 +0700

    Create UI and handle function for order summary page

---
+++ src/pages/order-summary/OrderSummaryPage.jsx
@@ -0,0 +1,51 @@
+import { Button, Card, Checkbox, List, Space, Typography } from 'antd';
+import { useState } from 'react';
+
+const OrderSummaryPage = () => {
+  const scoops = { total: 6, items: ['3 Vanilla'] };
+  const toppings = { total: 4.5, items: ['M&MA', 'Hot fudge', 'Gummy bear'] };
+  const [canSubmit, setCanSubmit] = useState(false);
+
+  const handleChange = () => {
+    setCanSubmit(!canSubmit);
+  };
+
+  return (
+    <Card title="Order Summary" bordered={false}>
+      <Space direction="vertical" size="middle" style={{ width: '100%' }}>
+        <List
+          header={<h2>Scoops: ${scoops.total.toFixed(2)}</h2>}
+          bordered
+          dataSource={scoops.items}
+          renderItem={(item) => (
+            <List.Item>
+              <Typography.Text>{item}</Typography.Text>
+            </List.Item>
+          )}
+        />
+        <List
+          header={<h2>Toppings: ${toppings.total.toFixed(2)}</h2>}
+          bordered
+          dataSource={toppings.items}
+          renderItem={(item) => (
+            <List.Item>
+              <Typography.Text>{item}</Typography.Text>
+            </List.Item>
+          )}
+        />
+        <Typography.Title>
+          Total ${(scoops.total + toppings.total).toFixed(2)}
+        </Typography.Title>
+        <Checkbox onChange={handleChange}>
+          I agree to&nbsp;
+          <Typography.Link href="">Terms and Conditions</Typography.Link>
+        </Checkbox>
+        <Button disabled={!canSubmit} type="primary">
+          Confirm order
+        </Button>
+      </Space>
+    </Card>
+  );
+};
+
+export default OrderSummaryPage;
